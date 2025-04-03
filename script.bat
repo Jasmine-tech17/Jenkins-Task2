1dccbff9ffd8c30fe459dc80467c852aab893af4
Jasmine-tech17 <jasminechaudhary1997@gmail.com>
2025-04-03 23:44:46 +0530

    updated bat

--- script.bat
+++ script.bat
@@ -1,7 +1,2 @@
 
 echo "Hello Jenkins!, This is automated build test"
-New Changes
-New Changes attempt 2
-New Changes attempt via pipeline creation
-New Changes attempt via jenkins config
-converted to bat
